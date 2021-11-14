a1f708e66752f200915380c80ed79d7093cfc801
MateMar04 <mateo.marchisone@gmail.com>
2021-11-14 08:54:51 -0300

    Designed window

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Calculadora {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton xButton;
+    private JButton cButton;
+    private JButton button5;
+    private JButton button6;
+    private JButton ansButton;
+    private JButton button8;
+}
